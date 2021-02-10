187ba6cf8ec56c324949dad68166efbed775d12e
Giray <giray@storyofams.com>
2021-02-10 15:15:42 +0100

    chore(pipes): remove default applied pipes

--- lib/pipes/applyInternalPipes.ts
+++
@@ -1,6 +0,0 @@
-import { ParseBooleanPipe } from './parseBoolean.pipe';
-import { ParseNumberPipe } from './parseNumber.pipe';
-
-export function applyInternalPipes(value: any): number | boolean {
-  return ParseNumberPipe(ParseBooleanPipe(value));
-}
