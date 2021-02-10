bab3a0730bd240cada9913cf0ae87185dac01172
ggurkal <giraygurkal@gmail.com>
2021-02-10 23:06:47 +0100

    chore: export request and response decorators

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,21 @@
 import 'reflect-metadata';
 
 export * from './createHandler';
-export { Body, Delete, Get, Header, HttpCode, HttpVerb, Post, Put, Query, SetHeader } from './decorators';
+export {
+  Body,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  HttpVerb,
+  Post,
+  Put,
+  Query,
+  SetHeader,
+  Req,
+  Request,
+  Res,
+  Response
+} from './decorators';
 export * from './exceptions';
 export * from './pipes';
