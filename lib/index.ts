23b16df0d89263f2e6b0a7e85a67b04aaf6911b4
Giray <giray@storyofams.com>
2021-02-10 15:16:28 +0100

    chore: export built-in pipes

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,4 @@ import 'reflect-metadata';
 export * from './createHandler';
 export { Body, Delete, Get, Header, HttpCode, HttpVerb, Post, Put, Query, SetHeader } from './decorators';
 export * from './exceptions';
+export * from './pipes';
