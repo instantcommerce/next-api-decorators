912375f4815c1990d0702c7244bdd57b87341098
ggurkal <giraygurkal@gmail.com>
2021-02-20 14:19:14 +0100

    refactor: move body validation to its own pipe

---
+++ lib/pipes/validation.pipe.ts
@@ -0,0 +1,18 @@
+import type { ClassTransformOptions } from 'class-transformer';
+import type { ValidatorOptions } from 'class-validator';
+import { validateObject } from '../internals/classValidator';
+import type { ParameterPipe, PipeMetadata } from './ParameterPipe';
+
+export interface ValidationPipeOptions extends ValidatorOptions {
+  transformOptions?: ClassTransformOptions;
+}
+
+export function ValidationPipe(options?: ValidationPipeOptions): ParameterPipe<any> {
+  return (value: any, metadata?: PipeMetadata) => {
+    if (!metadata?.metaType) {
+      return value;
+    }
+
+    return validateObject(metadata?.metaType, value, options);
+  };
+}
