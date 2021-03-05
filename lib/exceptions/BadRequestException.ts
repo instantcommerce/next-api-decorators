0bd73b20bccc38eb3a022661e128ee2ec49e4ffb
Doeke Leeuwis <leeuwis@amsdesign.nl>
2021-03-05 11:02:49 +0100

    refactor(exceptions): added default error messages

--- lib/exceptions/BadRequestException.ts
+++ lib/exceptions/BadRequestException.ts
@@ -3,7 +3,7 @@ import { HttpException } from './HttpException';
 export class BadRequestException extends HttpException {
   public name = 'BadRequestException';
 
-  public constructor(message?: string, errors?: string[]) {
+  public constructor(message: string = 'Bad request', errors?: string[]) {
     super(400, message, errors);
   }
 }
--- lib/exceptions/NotFoundException.ts
+++ lib/exceptions/NotFoundException.ts
@@ -3,7 +3,7 @@ import { HttpException } from './HttpException';
 export class NotFoundException extends HttpException {
   public name = 'NotFoundException';
 
-  public constructor(message?: string) {
+  public constructor(message: string = 'Not found') {
     super(404, message);
   }
 }
