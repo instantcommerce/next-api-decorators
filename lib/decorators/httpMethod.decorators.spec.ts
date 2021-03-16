b9b630c63847f3d4047c4b1b4bccdda864623147
ggurkal <giraygurkal@gmail.com>
2021-03-16 14:03:17 +0100

    refactor(decorators): add "path" option to http method decorators

--- lib/decorators/httpMethod.decorators.spec.ts
+++ lib/decorators/httpMethod.decorators.spec.ts
@@ -1,33 +1,63 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import 'reflect-metadata';
 import { Delete, Get, HTTP_METHOD_TOKEN, HttpVerb, Post, Put } from './httpMethod.decorators';
 
 class Test {
   @Get()
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   public get(): void {}
 
   @Post()
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   public post(): void {}
 
   @Put()
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   public put(): void {}
 
   @Delete()
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   public delete(): void {}
 }
 
-it('Should set the HttpMethod decorator.', () => {
-  const meta = Reflect.getMetadata(HTTP_METHOD_TOKEN, Test);
-  expect(meta).toBeInstanceOf(Map);
-  expect(meta).toMatchObject(
-    new Map([
-      [HttpVerb.DELETE, 'delete'],
-      [HttpVerb.GET, 'get'],
-      [HttpVerb.POST, 'post'],
-      [HttpVerb.PUT, 'put']
-    ])
-  );
+class TestPath {
+  @Get()
+  public index() {}
+
+  @Get('/explore')
+  public explore() {}
+
+  @Get('/explore/:id')
+  public exploreDetails() {}
+
+  @Get('/explore/:id/comments')
+  public exploreDetailsComments() {}
+
+  @Get('/explore/:id/comments/:commentId')
+  public exploreDetailsCommentDetails() {}
+}
+
+describe('HttpMethod decorator', () => {
+  it('Should create all the verbs.', () => {
+    const meta = Reflect.getMetadata(HTTP_METHOD_TOKEN, Test);
+    expect(meta).toBeInstanceOf(Array);
+    expect(meta).toMatchObject(
+      expect.arrayContaining([
+        { path: '/', verb: HttpVerb.GET, propertyKey: 'get' },
+        { path: '/', verb: HttpVerb.POST, propertyKey: 'post' },
+        { path: '/', verb: HttpVerb.PUT, propertyKey: 'put' },
+        { path: '/', verb: HttpVerb.DELETE, propertyKey: 'delete' }
+      ])
+    );
+  });
+
+  it('Should create the GET verb with paths', () => {
+    const meta = Reflect.getMetadata(HTTP_METHOD_TOKEN, TestPath);
+    expect(meta).toBeInstanceOf(Array);
+    expect(meta).toMatchObject(
+      expect.arrayContaining([
+        { path: '/', verb: HttpVerb.GET, propertyKey: 'index' },
+        { path: '/explore', verb: HttpVerb.GET, propertyKey: 'explore' },
+        { path: '/explore/:id', verb: HttpVerb.GET, propertyKey: 'exploreDetails' },
+        { path: '/explore/:id/comments', verb: HttpVerb.GET, propertyKey: 'exploreDetailsComments' },
+        { path: '/explore/:id/comments/:commentId', verb: HttpVerb.GET, propertyKey: 'exploreDetailsCommentDetails' }
+      ])
+    );
+  });
 });
