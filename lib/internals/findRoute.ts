8ad7ecb260817f7522956f45aea56e76e11e9d05
ggurkal <giraygurkal@gmail.com>
2021-03-16 14:03:52 +0100

    feat: route finder

---
+++ lib/internals/findRoute.ts
@@ -0,0 +1,32 @@
+import { Key } from 'path-to-regexp';
+import { HandlerMethod, HTTP_METHOD_TOKEN } from '../decorators';
+import { loadPackage } from './loadPackage';
+
+export function findRoute(
+  cls: Record<string, any>,
+  verb: string,
+  path: string
+): [Key[], RegExpExecArray | null | undefined, HandlerMethod | undefined] {
+  const methods: Array<HandlerMethod> = Reflect.getMetadata(HTTP_METHOD_TOKEN, cls);
+
+  const { pathToRegexp } = loadPackage('path-to-regexp');
+  if (!pathToRegexp) {
+    return [[], undefined, methods.find(f => f.path === '/' && f.verb === verb)];
+  }
+
+  const keys: Key[] = [];
+  let match: RegExpExecArray | null | undefined;
+  const method = methods.find((f: any) => {
+    match = pathToRegexp(f.path, keys).exec(path);
+
+    const condition = f.verb === verb && match?.length;
+
+    if (!condition) {
+      keys.length = 0;
+    }
+
+    return condition;
+  });
+
+  return [keys, match, method];
+}
