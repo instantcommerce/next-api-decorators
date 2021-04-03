78079395c4093c1152c9dbe683a052dc1016ded6
ggurkal <giraygurkal@gmail.com>
2021-04-04 01:18:15 +0200

    docs(pipes): add documentation for pipe metadata and options

--- lib/pipes/ParameterPipe.ts
+++ lib/pipes/ParameterPipe.ts
@@ -1,9 +1,16 @@
+/**
+ * Metadata definition for pipes.
+ */
 export interface PipeMetadata<T = any> {
+  /** Underlying base type (e.g `String`) of the parameter. */
   readonly metaType?: T;
+
+  /** Query parameter key or route parameter name of the parameter. */
   readonly name?: string;
 }
 
 export interface PipeOptions {
+  /* Determines whether field is nullable. */
   readonly nullable?: boolean;
 }
 
