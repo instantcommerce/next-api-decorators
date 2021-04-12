2487ac39c0031c16112f9b34e380ffb56231fbf6
ggurkal <giraygurkal@gmail.com>
2021-04-12 18:40:33 +0200

    refactor: change validator file name to be more generic

--- lib/pipes/validators/validation.pipe.ts
+++ lib/pipes/validators/validation.pipe.ts
@@ -1,6 +1,6 @@
 import type { ClassTransformOptions } from 'class-transformer';
 import type { ValidatorOptions } from 'class-validator';
-import { validateObject } from '../../internals/classValidator';
+import { validateObject } from '../../internals/validateObject';
 import type { ParameterPipe, PipeMetadata } from '../ParameterPipe';
 
 export interface ValidationPipeOptions extends ValidatorOptions {
