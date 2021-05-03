f534cd8a908a264ff8a878e094e4c3e64272298f
ggurkal <giraygurkal@gmail.com>
2021-05-03 19:25:22 +0200

    test: simple sub routes

---
+++ test/e2e-basic-routing.test.ts
@@ -0,0 +1,82 @@
+import 'reflect-metadata';
+import request from 'supertest';
+import { createHandler, Get, Param, ParseNumberPipe } from '../lib';
+import * as lp from '../lib/internals/loadPackage';
+import { setupServer } from './setupServer';
+
+const ARTICLES = [
+  { id: 1, title: 'Article 1' },
+  { id: 2, title: 'Article 2' }
+];
+const COMMENTS = [{ text: 'Comment 1' }, { text: 'Comment 2' }];
+
+class ArticleHandler {
+  @Get('/')
+  public articles(): Record<string, any>[] {
+    return ARTICLES;
+  }
+
+  @Get('/:id(\\d+)')
+  public article(@Param('id', ParseNumberPipe) id: number) {
+    return ARTICLES.find(article => article.id === id);
+  }
+
+  @Get('/comments')
+  public articleComments(): Record<string, any>[] {
+    return COMMENTS;
+  }
+
+  @Get('/comments/favorites')
+  public favoriteArticleComments(): Record<string, any>[] {
+    return [COMMENTS[0]];
+  }
+}
+
+describe('E2E - Basic routing', () => {
+  let server: ReturnType<typeof setupServer>;
+  beforeAll(() => (server = setupServer(createHandler(ArticleHandler))));
+  afterAll(() => {
+    if ('close' in server && typeof server.close === 'function') {
+      server.close();
+    }
+  });
+
+  it('Should return the articles.', () => request(server).get('/api/articles').expect(200, ARTICLES));
+
+  it('Should return a single article', () => request(server).get('/api/articles/1').expect(200, ARTICLES[0]));
+
+  it('Should return the article comments.', () => request(server).get('/api/articles/comments').expect(200, COMMENTS));
+
+  it('Should return the favorite article comments.', () =>
+    request(server).get('/api/articles/comments/favorites').expect(200, [COMMENTS[0]]));
+});
+
+describe('E2E - Basic routing (without "path-to-regexp")', () => {
+  let server: ReturnType<typeof setupServer>;
+  beforeAll(() => {
+    jest
+      .spyOn(lp, 'loadPackage')
+      .mockImplementation((name: string) => (name === 'path-to-regexp' ? false : require(name)));
+
+    server = setupServer(createHandler(ArticleHandler));
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+
+    if ('close' in server && typeof server.close === 'function') {
+      server.close();
+    }
+  });
+
+  it('Should return the articles when "path-to-regexp" is not installed.', () =>
+    request(server).get('/api/articles').expect(200, ARTICLES));
+
+  it('Should execute "articles" method when "path-to-regexp" is not installed.', () =>
+    request(server).get('/api/articles/1').expect(200, ARTICLES));
+
+  it('Should return the article comments when "path-to-regexp" is not installed.', () =>
+    request(server).get('/api/articles/comments').expect(200, COMMENTS));
+
+  it('Should return the favorite article when "path-to-regexp" is not installed.', () =>
+    request(server).get('/api/articles/comments/favorites').expect(200, [COMMENTS[0]]));
+});
