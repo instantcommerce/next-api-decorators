53737d2beed2e7071643ce812d6d0235c9cf2af6
ggurkal <giraygurkal@gmail.com>
2021-06-19 11:49:10 +0200

    fix: walk through url parts for nested folder routes

--- lib/createHandler.ts
+++ lib/createHandler.ts
@@ -3,6 +3,29 @@ import { findRoute } from './internals/findRoute';
 import { getParams } from './internals/getParams';
 import { notFound } from './internals/notFound';
 
+function walkUrl(classRef: new (...args: any[]) => any, url: string, method: string): ReturnType<typeof findRoute> {
+  let sliceStart = 1;
+
+  function walker(): ReturnType<typeof findRoute> {
+    url = url.split('/').slice(sliceStart).join('/');
+
+    if (!url.startsWith('/')) {
+      url = '/' + url;
+    }
+
+    const result = findRoute(classRef, method, url);
+
+    if (!result[2] && sliceStart < 5) {
+      sliceStart++;
+      return walker();
+    }
+
+    return result;
+  }
+
+  return walker();
+}
+
 /**
  * Prepares a router for the given class.
  *
@@ -30,10 +53,9 @@ export function createHandler(cls: new (...args: any[]) => any): NextApiHandler 
       return notFound(req, res);
     }
 
-    const [, , , ...path] = req.url.split('?')[0].split('/');
-    const joinedPath = `/${path.join('/')}`;
-
-    const [keys, match, method] = findRoute(cls, req.method, joinedPath);
+    // Remove the string `api` and the directory name from the url.
+    const clearedPath = req.url.split('?')[0].split('/').slice(2).join('/');
+    const [keys, match, method] = walkUrl(cls, clearedPath, req.method);
     if (!method) {
       return notFound(req, res);
     }
