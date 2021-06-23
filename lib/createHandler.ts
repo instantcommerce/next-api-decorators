4c663c247d74d87e0b6bb3844b8feda7e6de00ac
ggurkal <giraygurkal@gmail.com>
2021-06-23 15:39:46 +0200

    chore: move url handling to its own file

--- lib/createHandler.ts
+++ lib/createHandler.ts
@@ -2,12 +2,13 @@ import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import { findRoute } from './internals/findRoute';
 import { getParams } from './internals/getParams';
 import { notFound } from './internals/notFound';
+import { parseRequestUrl } from './internals/parseRequestUrl';
 
 interface CreateHandlerOptions {
   /**
    * Directory path of the handler file. Directly pass the internal `__dirname`.
    */
-  dirName?: string;
+  directoryPath?: string;
 }
 
 /**
@@ -37,20 +38,8 @@ export function createHandler(cls: new (...args: any[]) => any, options?: Create
       return notFound(req, res);
     }
 
-    let clearedPath = req.url.split('?')[0].split('/').slice(3).join('/');
-
-    if (options?.dirName) {
-      const pathRegExp = new RegExp(options.dirName.split('/.next/server/pages')[1].replace(/(\[\w+\])/, '(\\w+)'));
-      if (pathRegExp.test(req.url)) {
-        clearedPath = req.url.replace(pathRegExp, '');
-      }
-    }
-
-    if (!clearedPath.startsWith('/')) {
-      clearedPath = '/' + clearedPath;
-    }
-
-    const [keys, match, method] = findRoute(cls, req.method, clearedPath);
+    const path = parseRequestUrl(req.url, options?.directoryPath);
+    const [keys, match, method] = findRoute(cls, req.method, path);
     if (!method) {
       return notFound(req, res);
     }
