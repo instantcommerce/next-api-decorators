1892fa783cf03d45fa2c8abf373b6473a2a8cd80
ggurkal <giraygurkal@gmail.com>
2021-06-23 10:24:56 +0200

    fix: handlers in nested folders

--- lib/createHandler.ts
+++ lib/createHandler.ts
@@ -3,27 +3,11 @@ import { findRoute } from './internals/findRoute';
 import { getParams } from './internals/getParams';
 import { notFound } from './internals/notFound';
 
-function walkUrl(classRef: new (...args: any[]) => any, url: string, method: string): ReturnType<typeof findRoute> {
-  let sliceStart = 1;
-
-  function walker(): ReturnType<typeof findRoute> {
-    url = url.split('/').slice(sliceStart).join('/');
-
-    if (!url.startsWith('/')) {
-      url = '/' + url;
-    }
-
-    const result = findRoute(classRef, method, url);
-
-    if (!result[2] && sliceStart < 5) {
-      sliceStart++;
-      return walker();
-    }
-
-    return result;
-  }
-
-  return walker();
+interface CreateHandlerOptions {
+  /**
+   * Directory path of the handler file. Directly pass the internal `__dirname`.
+   */
+  dirName?: string;
 }
 
 /**
@@ -45,7 +29,7 @@ function walkUrl(classRef: new (...args: any[]) => any, url: string, method: str
  * export default createHandler(Events);
  * ```
  */
-export function createHandler(cls: new (...args: any[]) => any): NextApiHandler {
+export function createHandler(cls: new (...args: any[]) => any, options?: CreateHandlerOptions): NextApiHandler {
   const instance = new cls();
 
   return (req: NextApiRequest, res: NextApiResponse) => {
@@ -53,9 +37,20 @@ export function createHandler(cls: new (...args: any[]) => any): NextApiHandler 
       return notFound(req, res);
     }
 
-    // Remove the string `api` and the directory name from the url.
-    const clearedPath = req.url.split('?')[0].split('/').slice(2).join('/');
-    const [keys, match, method] = walkUrl(cls, clearedPath, req.method);
+    let clearedPath = req.url.split('?')[0].split('/').slice(3).join('/');
+
+    if (options?.dirName) {
+      const pathRegExp = new RegExp(options.dirName.split('/.next/server/pages')[1].replace(/(\[\w+\])/, '(\\w+)'));
+      if (pathRegExp.test(req.url)) {
+        clearedPath = req.url.replace(pathRegExp, '');
+      }
+    }
+
+    if (!clearedPath.startsWith('/')) {
+      clearedPath = '/' + clearedPath;
+    }
+
+    const [keys, match, method] = findRoute(cls, req.method, clearedPath);
     if (!method) {
       return notFound(req, res);
     }
