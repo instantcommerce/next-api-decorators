9b23dd6e30ac4a33394a5c6d9510c9b839a0b70d
ggurkal <giraygurkal@gmail.com>
2021-08-18 10:00:53 +0200

    test: update next's apiResolver function path
    [skip ci]

--- test/setupServer.ts
+++ test/setupServer.ts
@@ -3,7 +3,7 @@ import http from 'http';
 import { parse } from 'url';
 import express from 'express';
 import type { NextApiHandler } from 'next';
-import { apiResolver } from 'next/dist/next-server/server/api-utils';
+import { apiResolver } from 'next/dist/server/api-utils';
 
 export function setupServer(handler: NextApiHandler, disableBodyParser?: boolean): http.Server | express.Express {
   if (process.env.TEST_SERVER === 'nextjs') {
